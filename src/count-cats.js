cf3c52fb1eee670b2f944e36675f3a59b17494e7
Marat Maksumov <maxumov@gmail.com>
2021-02-17 13:11:35 +0300

    feat: add count cats! subtask solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,26 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+/**
+ * Your task is to count the cats hidden in the backyard
+ * (presented by two-dimensional Array). Cats hide well,
+ * but their ears ("^^") are clearly visible.
+ * Your task is to implement the countCats(backyard)
+ * function that will count cats. Good luck!
+ *
+ * Number of cats found should be number. If no cats found,
+ * function should return 0.
+ *
+ * For example:
+ *
+ * countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3
+ */
+
+module.exports = countCats = (backyard) =>
+  backyard.reduce(
+    (cats, place) =>
+      (cats += place.reduce(
+        (catsInPlace, isItCat) => (catsInPlace += isItCat === '^^' ? 1 : 0),
+        0
+      )),
+    0
+  );
